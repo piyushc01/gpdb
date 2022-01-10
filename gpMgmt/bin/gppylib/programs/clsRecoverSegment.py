02a32ae1bc5ca5b6cb3927d01cffed9aa16cc8e4
Piyush Chandwadkar <pchandwadkar@vmware.com>
2022-01-10 18:25:42 +0530

    Test changes

--- gpMgmt/bin/gppylib/programs/clsRecoverSegment.py
+++ gpMgmt/bin/gppylib/programs/clsRecoverSegment.py
@@ -305,10 +305,10 @@ class GpRecoverSegmentProgram:
         from pprint import PrettyPrinter
         pp = PrettyPrinter(indent=2)
         logger.debug("PIYUSH: Before getRecoveryBasedOnOptons")
-        logger.debug(pp.pformat(gpArray.__repr__()))
+        logger.debug(pp.pformat(vars(gpArray)))
         mirrorBuilder = self.getRecoveryActionsBasedOnOptions(gpEnv, gpArray)
         logger.debug("PIYUSH: After getRecoveryBasedOnOptons")
-        logger.debug(pp.pformat(gpArray.__repr__()))
+        logger.debug(pp.pformat(vars(gpArray)))
 
         if self.__options.outputSampleConfigFile is not None:
             # just output config file and done
